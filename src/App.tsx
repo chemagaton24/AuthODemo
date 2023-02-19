f3e40692d544aa3f81bfb1a714903737cad81dff
chemagaton24 <chembeeragaton@gmail.com>
2023-02-19 14:19:14 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Auth0Provider } from "@auth0/auth0-react";
+import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import Profile from "./Profile";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Auth0Provider
+      domain="chembeeragaton.jp.auth0.com"
+      clientId="ZfN660B0ucf4jCIUDTD01GFzwq29RMuJ"
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <LoginButton />
+      <LogoutButton />
+      <Profile />
+    </Auth0Provider>
   );
-}
+};
 
 export default App;
