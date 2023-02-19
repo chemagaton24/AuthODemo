0434dd0bc7201ddae9ba05af8b5df4029e283a79
chemagaton24 <chembeeragaton@gmail.com>
2023-02-19 14:42:30 +0800

    sd

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,13 @@ const App = () => {
       domain="chembeeragaton.jp.auth0.com"
       clientId="ZfN660B0ucf4jCIUDTD01GFzwq29RMuJ"
       authorizationParams={{
-        // redirect_uri: window.location.origin,
-        redirect_uri: "https://chemagaton24.github.io/AuthODemo",
+        redirect_uri: window.location.origin,
+        // redirect_uri: "https://chemagaton24.github.io/AuthODemo",
       }}
     >
       <LoginButton />
       <LogoutButton />
       <Profile />
-      xcxcx
     </Auth0Provider>
   );
 };
